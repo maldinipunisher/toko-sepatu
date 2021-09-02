40b8acabdc8410f28af751ad07e5d629d90eac32
kamal3356 <latifulkamal2000@gmail.com>
2021-09-02 15:49:43 +0700

    v0.22

---
+++ resources/views/layouts/verification.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="id">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="{{asset('css/app.css')}}">
+    <link rel="stylesheet" href="{{ asset('bootstrap-icons/font/bootstrap-icons.css') }}">
+    <title>Verification</title>
+</head>
+
+<body>
+    <!-- navbar  -->
+<nav class="navbar navbar-light bg-light">
+  <div class="container">
+    <a class="navbar-brand text-primary" href="#">
+    <b>TOKO SEPATU</b>
+    </a>
+  </div>
+</nav>
+<!-- card -->
+    <div class="card position-absolute top-50 start-50 translate-middle" style="width: 30rem ;">
+        <ul class="list-group list-group-flush ">
+            <li class="list-group-item bg-secondary text-black">Verify Your Email Address</li>
+            <li class="list-group-item">Before proceeding, Please check Your Email for a Verification , If you did not receive the email,
+                <a href="">click here</a>
+</li>
+        </ul>
+    </div>
+
+    <script src="{{ asset('js/bootstrap.bundle.js') }}"></script>
+
+</body>
+
+</html>
